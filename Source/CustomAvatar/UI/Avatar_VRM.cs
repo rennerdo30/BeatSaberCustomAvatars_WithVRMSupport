f8f0ec0240a6b0b3574d64305addfa37172be221
Graham Ransom <graham_ransom@hotmail.com>
2022-11-13 19:00:28 +0000

    better naming of unlicenced files

--- Source/CustomAvatar/UI/Avatar_VRM.cs
+++ Source/CustomAvatar/UI/Avatar_VRM.cs
@@ -44,108 +44,108 @@ namespace VRMAvatar
             }
         }
 
-        public struct Finger
-        {
-            public Quaternion one;
-            public Quaternion two;
-            public Quaternion three;
+            public struct Finger
+    {
+        public Quaternion one;
+        public Quaternion two;
+        public Quaternion three;
 
-            public Finger(Quaternion one, Quaternion two, Quaternion three)
+        public Finger(Quaternion one, Quaternion two, Quaternion three)
+        {
+            this.one = one;
+            this.two = two;
+            this.three = three;
+        }
+    }
+    class HandPositionConstants
+    {
+        public static Finger index = new Finger(
+                                                new Quaternion(0.008440408f, -0.001334298f, 0.2419432f, 0.9702529f),
+                                                new Quaternion(0.006469311f, -0.005582907f, 0.7170327f, 0.6969872f),
+                                                new Quaternion(0.005353101f, -0.006660718f, 0.8312484f, 0.5558357f)
+                                                );
+        public static Finger little = new Finger(
+                                                new Quaternion(0.008255607f, -0.00205866f, 0.241947f, 0.9702522f),
+                                                new Quaternion(0.005930441f, -0.006101066f, 0.7170366f, 0.6969836f),
+                                                new Quaternion(0.004364784f, -0.007303547f, 0.8583599f, 0.5129775f)
+                                                );
+        public static Finger middle = new Finger(
+                                                new Quaternion(0.008255607f, -0.00205866f, 0.241947f, 0.9702522f),
+                                                new Quaternion(0.005930441f, -0.006101066f, 0.7170366f, 0.6969836f),
+                                                new Quaternion(0.001539939f, -0.0083679f, 0.98344f, 0.1809834f)
+                                                );
+        public static Finger ring = new Finger(
+                                                new Quaternion(0.008255607f, -0.00205866f, 0.241947f, 0.9702522f),
+                                                new Quaternion(0.005930441f, -0.006101066f, 0.7170366f, 0.6969836f),
+                                                new Quaternion(0.002387176f, -0.008166672f, 0.9597998f, 0.2805563f)
+                                                );
+        public static Finger thumb = new Finger(
+                                                new Quaternion(0.5142931f, -0.0385387f, -0.02314265f, 0.8564355f),
+                                                new Quaternion(0.502104f, -0.1893172f, -0.1136858f, 0.8361376f),
+                                                new Quaternion(0.4902184f, -0.2618173f, -0.1572224f, 0.8163448f)
+                                                );
+        public static Vector3 handOffset = new Vector3(0.0873f, 0.0348f, 0f);
+
+        private static Quaternion Reflect(Quaternion quat, bool reflect)
+        {
+            if (!reflect)
             {
-                this.one = one;
-                this.two = two;
-                this.three = three;
+                return quat;
+            }
+            else
+            {
+                Quaternion temp = quat;
+                temp.z = -temp.z;
+                temp.y = -temp.y;
+                return temp;
             }
         }
-        class HandPositionConstants
+
+        public static void ApplyToHand(Transform hand, bool right)
         {
-            public static Finger index = new Finger(
-                                                    new Quaternion(0.008440408f, -0.001334298f, 0.2419432f, 0.9702529f),
-                                                    new Quaternion(0.006469311f, -0.005582907f, 0.7170327f, 0.6969872f),
-                                                    new Quaternion(0.005353101f, -0.006660718f, 0.8312484f, 0.5558357f)
-                                                    );
-            public static Finger little = new Finger(
-                                                    new Quaternion(0.008255607f, -0.00205866f, 0.241947f, 0.9702522f),
-                                                    new Quaternion(0.005930441f, -0.006101066f, 0.7170366f, 0.6969836f),
-                                                    new Quaternion(0.004364784f, -0.007303547f, 0.8583599f, 0.5129775f)
-                                                    );
-            public static Finger middle = new Finger(
-                                                    new Quaternion(0.008255607f, -0.00205866f, 0.241947f, 0.9702522f),
-                                                    new Quaternion(0.005930441f, -0.006101066f, 0.7170366f, 0.6969836f),
-                                                    new Quaternion(0.001539939f, -0.0083679f, 0.98344f, 0.1809834f)
-                                                    );
-            public static Finger ring = new Finger(
-                                                    new Quaternion(0.008255607f, -0.00205866f, 0.241947f, 0.9702522f),
-                                                    new Quaternion(0.005930441f, -0.006101066f, 0.7170366f, 0.6969836f),
-                                                    new Quaternion(0.002387176f, -0.008166672f, 0.9597998f, 0.2805563f)
-                                                    );
-            public static Finger thumb = new Finger(
-                                                    new Quaternion(0.5142931f, -0.0385387f, -0.02314265f, 0.8564355f),
-                                                    new Quaternion(0.502104f, -0.1893172f, -0.1136858f, 0.8361376f),
-                                                    new Quaternion(0.4902184f, -0.2618173f, -0.1572224f, 0.8163448f)
-                                                    );
-            public static Vector3 handOffset = new Vector3(0.0873f, 0.0348f, 0f);
+            if (hand == null)
+                return; //nothing can be done.
 
-            private static Quaternion Reflect(Quaternion quat, bool reflect)
+            //NOTE: Hand rotation can't be done here as the Tracking Device is mapped directly to the hand, overwriting rotations.
+
+            int nFingers = 5;
+            if (hand.GetChildCount() < nFingers)
+                nFingers = hand.GetChildCount();
+
+            for (int i = 0; i < nFingers; i++)
             {
-                if (!reflect)
+                Transform fingey = hand.GetChild(i);
+
+                if (fingey == null)
+                    continue; //nothing can be done.
+
+                Finger fingerThing = new Finger(Quaternion.identity, Quaternion.identity, Quaternion.identity);
+                switch (i)
                 {
-                    return quat;
+                    case 0:
+                        fingerThing = index;
+                        break;
+                    case 1:
+                        fingerThing = little;
+                        break;
+                    case 2:
+                        fingerThing = middle;
+                        break;
+                    case 3:
+                        fingerThing = ring;
+                        break;
+                    case 4:
+                        fingerThing = thumb;
+                        break;
+                    default:
+                        break;
                 }
-                else
-                {
-                    Quaternion temp = quat;
-                    temp.z = -temp.z;
-                    temp.y = -temp.y;
-                    return temp;
-                }
-            }
-
-            public static void ApplyToHand(Transform hand, bool right)
-            {
-                if (hand == null)
-                    return; //nothing can be done.
-
-                //NOTE: Hand rotation can't be done here as the Tracking Device is mapped directly to the hand, overwriting rotations.
-
-                int nFingers = 5;
-                if (hand.GetChildCount() < nFingers)
-                    nFingers = hand.GetChildCount();
-
-                for (int i = 0; i < nFingers; i++)
-                {
-                    Transform fingey = hand.GetChild(i);
-
-                    if (fingey == null)
-                        continue; //nothing can be done.
-
-                    Finger fingerThing = new Finger(Quaternion.identity, Quaternion.identity, Quaternion.identity);
-                    switch (i)
-                    {
-                        case 0:
-                            fingerThing = index;
-                            break;
-                        case 1:
-                            fingerThing = little;
-                            break;
-                        case 2:
-                            fingerThing = middle;
-                            break;
-                        case 3:
-                            fingerThing = ring;
-                            break;
-                        case 4:
-                            fingerThing = thumb;
-                            break;
-                        default:
-                            break;
-                    }
-                    fingey.rotation = Reflect(fingerThing.one, right);
-                    fingey.GetChild(0).rotation = Reflect(fingerThing.two, right);
-                    fingey.GetChild(0).GetChild(0).rotation = Reflect(fingerThing.three, right);
-                }
+                fingey.rotation = Reflect(fingerThing.one, right);
+                fingey.GetChild(0).rotation = Reflect(fingerThing.two, right);
+                fingey.GetChild(0).GetChild(0).rotation = Reflect(fingerThing.three, right);
             }
         }
+    }
 
         private async static Task<AvatarPrefab> LoadVRM(string path, IProgress<float> progress, CancellationToken cancellationToken, Dictionary<string, Task<AvatarPrefab>> tasks, DiContainer _container)
         {
@@ -195,6 +195,7 @@ namespace VRMAvatar
             GameObject obj = null;
 
             {
+                Debug.Log("New VRM Avatar");
                 var avatar = new GameObject("Avatar");
                 GameObject.DontDestroyOnLoad(avatar.gameObject);
                 obj = avatar.gameObject;
@@ -246,8 +247,8 @@ namespace VRMAvatar
                 VRMMeta meta = instance.GetComponent<VRMMeta>();
                 if (meta == null)
                 {
-                    descriptor.name = "null";
-                    descriptor.author = "null";
+                    descriptor.name = "";
+                    descriptor.author = "";
                     descriptor.cover = null;
                 }
                 else
@@ -256,7 +257,13 @@ namespace VRMAvatar
                     descriptor.author = meta.Meta.Author;
                     if (meta.Meta.Thumbnail != null)
                         descriptor.cover = Sprite.Create(meta.Meta.Thumbnail, new Rect(0, 0, meta.Meta.Thumbnail.width, meta.Meta.Thumbnail.height), Vector2.zero);
+                    if (descriptor.name.Length == 0)
+                        descriptor.name = "";
                 }
+
+                if (descriptor.name == "")
+                    descriptor.name = System.IO.Path.GetFileName(path);
+
                 descriptor.allowHeightCalibration = true;
             }
 
@@ -293,13 +300,16 @@ namespace VRMAvatar
                 Task<AvatarPrefab> task;
                 if (____tasks.TryGetValue(fullPath, out task))
                 {
+                    Debug.Log($"Loading Existing VRM from cache {fullPath}");
+
                     __result = task;
                     return false;
                 }
 
+                Debug.Log($"Loading New VRM from File {fullPath}");
                 task = LoadVRM(fullPath, progress, cancellationToken, ____tasks, ____container);
 
-                ____tasks.Add(fullPath, task);
+                //____tasks.Add(fullPath, task); //reload avatar from cache not working atm for some reason.
                 __result = task;
                 return false;
             }
